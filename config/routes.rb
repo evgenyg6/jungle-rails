f0dfe57b50265486fcf9dbeef0c6b52b63fc33d5
Evgenyg <evgenyg@live.com>
2017-10-13 16:54:56 -0600

    Added review and ratings function
    Can write a review, set a rating, and submit it to DB.

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,36 @@
 Rails.application.routes.draw do
 
+  resources :reviews, only: [:edit, :destroy, :update]
   resources :users
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
+
+  resources :products do
+    resources :reviews, only: [:create]
+  end
+
   resource :cart, only: [:show] do
     put    :add_item
     delete :remove_item
   end
 
-  resources :orders, only: [:create, :show]
-
-  get '/register' => 'users#new'
-
+  get('/register' => 'users#new')
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
+
+  resources :orders, only: [:create, :show]
 
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, except: [:edit, :update, :show]
+
   end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
