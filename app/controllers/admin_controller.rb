9fb8cc9cfc554756047b8ebb0ac73e2d497015b8
Evgenyg <evgenyg@live.com>
2017-10-13 10:41:27 -0600

    Merge branch 'feature/admin-basic-auth'
    Moved admin auth info to .env

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,3 +1,3 @@
 class AdminController < ApplicationController
-  http_basic_authenticate_with name: "Jungle", password: "book"
+  http_basic_authenticate_with name: ENV['SECRET_NAME'], password: ENV['SECRET_PASSWORD']
 end
