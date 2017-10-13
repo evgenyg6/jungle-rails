947e278f0d2bf2f727dcfebecf4768de7c0539af
Evgenyg <evgenyg@live.com>
2017-10-13 13:05:28 -0600

    Added temp reviews to validate their existance #2

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,9 +3,8 @@ class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :product
 
-  validates :product_id, presence: true
-  validates :user_id, presence: true
-  validates :description, presence: true
-  validates :rating, presence: true
-
+  validates_presence_of :product_id
+  validates_presence_of :user_id
+  validates_presence_of :description
+  validates_presence_of :rating
 end
